d85ec7483e9fba37a834f127c8ed2f69c9bd9c93
Metayean <i.meta.yyy@gmail.com>
2017-03-23 09:35:10 +0700

    23/03/57

--- server.js
+++ server.js
@@ -21,23 +21,24 @@ mongo.connect('mongodb://localhost/app', function (err,db){
 	io.on('connection', function(socket){
 
 		var col  = db.collection('event');
-		col.find().sort({_id: 1}).toArray(function(err,res){
-			if(err) throw err;
-			socket.emit('output',res);
-		});
+		// col.find().limit(100).sort({_id: 1}).toArray(function(err,res){
+		// 	if(err) throw err;
+		// 	socket.emit('output',res);
+		// 	// console.log(res);
+		// });
 
 		socket.on('input',function(data){
-			var id = data.id,
+			var token = data.id,
 				name = data.name,
 				picture = data.picture;
 
-			console.log('data');
-			col.insert({token: id, name: name, picture: picture}, function (){
+			// console.log('data');
+			col.insert({token: token, name: name, picture: picture}, function (){
 				console.log('Inserted');
 			});
 
 			io.emit('output',[data]);
-
+			// console.log([data]);
 		});
 
 		console.log('hello');
