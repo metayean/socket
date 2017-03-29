41744dd663d0fb4388964f1a0856d39e6313bdda
metayean <i.meta.yyy@gmail.com>
2017-03-29 16:50:11 +0700

    yes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var mongo = require('mongodb').MongoClient;
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+app.use(express.static(__dirname + ''));
 
 
 server.listen(8081);
